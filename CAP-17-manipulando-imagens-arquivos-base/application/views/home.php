828f1b3b728208511e9f359a643c5584426c10c4
JLamim Projetos Web & Mobile <dev@jlamim.com.br>
2016-03-14 22:14:38 -0300

    Ajuste frontend capítulo 17

--- CAP-17-manipulando-imagens-arquivos-base/application/views/home.php
+++ CAP-17-manipulando-imagens-arquivos-base/application/views/home.php
@@ -4,6 +4,19 @@
   <div class="page-header">
     <h1>Capítulo 17 - Manipulação de Imagens</h1>
   </div>
+  <div class="row">
+    <div class="col-md-12">
+      <form action="" method="POST" enctype="multipart/form-data">
+        <div class="form-group">
+          <label>Selecione uma imagem</label>
+          <input type="file" name="image" />
+        </div>
+        <div class="form-group">
+          <input type="submit" class="btn btn-success pull-right" value="Processar" />
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
 
 <?php $this->load->view('commons/footer'); ?>
